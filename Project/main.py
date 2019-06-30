1bbf494418613f861957f8a019a7b9a1e6d19aad
wertrain <wertrain@gmail.com>
2019-06-30 13:10:30 +0900

    環境設定

--- Project/main.py
+++ Project/main.py
@@ -1,8 +1,15 @@
 import os
 import sys
+import csv
 
 def main():
-    print ('hello')
+    with open('Project/data/Zaim.20190630115341.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        header = next(reader)  # ヘッダーを読み飛ばしたい時
+
+        for row in reader:
+            print (row)          # 1行づつ取得できる
+
 
 if __name__ == '__main__':
     main()
